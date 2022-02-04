c26d148875e649f45496e7da6986874c07daaf55
sagar23399 <sagarpatil23399@gmail.com>
2022-02-04 17:22:46 +0530

    add prompt component for Quote  form

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,10 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
